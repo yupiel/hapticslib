4547b4852fa6a4a218b4c0f332de28f4b119d353
Yupiel <stefan_mitrovic@outlook.com>
2024-08-06 18:59:40 +0200

    add some more lua types and make them const

--- src/lua_types.rs
+++ src/lua_types.rs
@@ -2,18 +2,25 @@
 
 use std::ffi::{c_double, c_int, c_void};
 
-pub static LUA_TNONE: c_int = -1;
-pub static LUA_TNIL: c_int = 0;
-pub static LUA_TBOOLEAN: c_int = 1;
-pub static LUA_TLIGHTUSERDATA: c_int = 2;
-pub static LUA_TNUMBER: c_int = 3;
-pub static LUA_TSTRING: c_int = 4;
-pub static LUA_TTABLE: c_int = 5;
-pub static LUA_TFUNCTION: c_int = 6;
-pub static LUA_TUSERDATA: c_int = 7;
-pub static LUA_TTHREAD: c_int = 8;
+pub const LUA_TNONE: c_int = -1;
+pub const LUA_TNIL: c_int = 0;
+pub const LUA_TBOOLEAN: c_int = 1;
+pub const LUA_TLIGHTUSERDATA: c_int = 2;
+pub const LUA_TNUMBER: c_int = 3;
+pub const LUA_TSTRING: c_int = 4;
+pub const LUA_TTABLE: c_int = 5;
+pub const LUA_TFUNCTION: c_int = 6;
+pub const LUA_TUSERDATA: c_int = 7;
+pub const LUA_TTHREAD: c_int = 8;
 
-pub static LUA_MINSTACK: c_int = 20;
+pub const LUA_MINSTACK: c_int = 20;
+
+pub const LUA_REGISTRYINDEX: c_int = -10000;
+pub const LUA_ENVIRONINDEX: c_int = -10001;
+pub const LUA_GLOBALSINDEX: c_int = -10002;
+pub const fn lua_upvalueindex(i: c_int) -> c_int {
+    LUA_GLOBALSINDEX - i
+}
 
 pub struct lua_State;
 pub type lua_Number = c_double;
