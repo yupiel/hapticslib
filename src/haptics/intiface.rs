f249ef449b9c586cec68b35147839f72a8820e86
Yupiel <stefan_mitrovic@outlook.com>
2024-08-16 20:24:50 +0200

    replace logs with rust logs

--- src/haptics/intiface.rs
+++ src/haptics/intiface.rs
@@ -1,8 +1,7 @@
 use std::{sync::mpsc::{Receiver, SendError}, thread};
 
 use buttplug::{client::{ButtplugClient, ButtplugClientError}, core::{connector::new_json_ws_client_connector, errors::ButtplugError}};
-
-use crate::superblt::pd2_logger::{PD2HOOK_LOG_ERROR, PD2HOOK_LOG_LOG};
+use log::{error, info};
 
 use super::channel::{set_or_update_haptics_sender, HapticsMessage, HAPTICS_SENDER};
 
@@ -57,79 +56,82 @@ pub fn haptics_spawn_thread(websocket_address: String, mpsc_receiver: Receiver<H
             let connector = new_json_ws_client_connector(&format!("ws://{}", websocket_address));
             let client = ButtplugClient::new("PD2 Heister's Haptics");
             
-            PD2HOOK_LOG_LOG!("Attempting connection to intiface...");
+            info!("Attempting connection to intiface...");
 
             if let Err(e) = client.connect(connector).await {
                 match e {
                     ButtplugClientError::ButtplugConnectorError(error) => {
-                        PD2HOOK_LOG_ERROR!("Can't connect to Intiface server. Dropping connection. Message: {}", error);
+                        error!("Can't connect to Intiface server. Dropping connection. Message: {}", error);
                         return;
                     }
                     ButtplugClientError::ButtplugError(error) => match error {
                         ButtplugError::ButtplugHandshakeError(error) => {
-                            PD2HOOK_LOG_ERROR!("Handshake issue with Intiface server. Dropping connection. Message: {}", error);
+                            error!("Handshake issue with Intiface server. Dropping connection. Message: {}", error);
                             return;
                         }
                         error => {
-                            PD2HOOK_LOG_ERROR!("Unexpected error when trying to connect to Intiface! Message: {}", error);
+                            error!("Unexpected error when trying to connect to Intiface! Message: {}", error);
                             return;
                         }
                     },
                 }
             }
 
-            PD2HOOK_LOG_LOG!("Connected to intiface! Scanning for devices...");
+            info!("Connected to intiface! Scanning for devices...");
 
             client.start_scanning().await.unwrap();
             client.stop_scanning().await.unwrap();
 
-            PD2HOOK_LOG_LOG!("Finished scanning. Listing devices.");
+            info!("Finished scanning. Listing devices.");
             for device in client.devices().iter() {
-                PD2HOOK_LOG_LOG!("Device {} [{}] found.", device.display_name().clone().unwrap_or("Unknown".into()), device.name());
+                info!("Device {} [{}] found.", device.display_name().clone().unwrap_or("Unknown".into()), device.name());
             }
 
             'main: loop {
                 match mpsc_receiver.recv() {
                     Ok(haptics_message) => {
                         if !client.connected() {
-                            PD2HOOK_LOG_ERROR!("Client lost connection to intiface WebSocket. Closing Thread.");
+                            error!("Client lost connection to intiface WebSocket. Closing Thread.");
                             break 'main;
                         }
 
                         match haptics_message {
                             HapticsMessage::Kill => {
                                 client.disconnect().await.inspect_err(|err| {
-                                    PD2HOOK_LOG_ERROR!("Disconnecting Heister's Haptics Client failed. Message: {}", err);
+                                    error!("Disconnecting Heister's Haptics Client failed. Message: {}", err);
                                 }).unwrap_or(());
 
-                                PD2HOOK_LOG_LOG!("Kill Signal received. Closing Thread.");
+                                info!("Kill Signal received. Closing Thread.");
                                 break 'main;
                             },
-                            HapticsMessage::Ping => {
-                                // This is just used to check if the main loop is still alive
-                                continue 'main;
+                            HapticsMessage::Ping => {/*  This is just used to check if the main loop is still alive */},
+                            HapticsMessage::ScanStart => {
+                                info!("Starting scanning for devices...");
+                                client.start_scanning().await.unwrap();
                             },
-                            HapticsMessage::ScanStart => todo!(),
-                            HapticsMessage::ScanStop => todo!(),
+                            HapticsMessage::ScanStop => {
+                                info!("Stopping scanning for devices.");
+                                client.stop_scanning().await.unwrap();
+                            },
                             HapticsMessage::StopAll => {
                                 for device in client.devices() {
                                     device.vibrate(&buttplug::client::ScalarValueCommand::ScalarValue(0_f64)).await.inspect_err(|err| {
-                                        PD2HOOK_LOG_ERROR!("Failed to send vibration command to device. Message: {}", err);
+                                        error!("Failed to send vibration command to device. Message: {}", err);
                                     }).unwrap_or(());
                                 }
                             },
                             HapticsMessage::Strength(strength) => {
                                 for device in client.devices() {
                                     device.vibrate(&buttplug::client::ScalarValueCommand::ScalarValue(strength)).await.inspect_err(|err| {
-                                        PD2HOOK_LOG_ERROR!("Failed to send vibration command to device. Message: {}", err);
+                                        error!("Failed to send vibration command to device. Message: {}", err);
                                     }).unwrap_or(());
                                 }
                             },
                         }
                     },
                     Err(_) => {
-                        PD2HOOK_LOG_ERROR!("Sender died... this shouldn't happen.");
-                        break;
+                        error!("Sender died... this shouldn't happen.");
+                        break 'main;
                     }
                 }
             }
